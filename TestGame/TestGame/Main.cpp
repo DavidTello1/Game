3089beb892109b5cb7e8602cb2920a563e0fd752
DavidTello1 <36265057+DavidTello1@users.noreply.github.com>
2018-03-06 19:14:01 +0100

    Fluid movement

--- TestGame/TestGame/Main.cpp
+++ TestGame/TestGame/Main.cpp
@@ -9,8 +9,9 @@ int main(int argc, char* argv[]) {
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_Window *window = SDL_CreateWindow("SDL2TEST", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL);
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	SDL_Rect ship = { 0, 0, 60, 60 };
-	int speed = 10;
+	SDL_Rect ship = { 200, 300, 50, 50 };
+	int speedx = 0, speedy = 0;
+
 	bool isRunning = true;
 	SDL_Event event;
 
@@ -19,52 +20,63 @@ int main(int argc, char* argv[]) {
 			if (event.type == SDL_QUIT) {
 				isRunning = false;
 			}
-			if (event.type == SDL_KEYDOWN) {
+			else if (event.type == SDL_KEYDOWN) {
 				switch (event.key.keysym.sym){
 				case SDLK_LEFT:
-					if (ship.x - speed > -10) {
-						ship.x -= speed;
-					}
+					speedx = -5;
 					break;
 				case SDLK_RIGHT:
-					if (ship.x + speed < 1200-50) {
-						ship.x += speed;
-					}
+					speedx = 5;
 					break;
 				case SDLK_UP:
-					if (ship.y - speed > -10) {
-						ship.y -= speed;
-					}
+					speedy = -5;
 					break;
 				case SDLK_DOWN:
-					if (ship.y + speed < 600-50) {
-						ship.y += speed;
-					}
+					speedy = 5;
+					break;
+				
+				case SDLK_SPACE:
+					;
+				}
+			}
+			if (event.type == SDL_KEYUP) {
+				switch (event.key.keysym.sym) {
+				case SDLK_LEFT:
+					if (speedx < 0) {
+						speedx = 0;	}
+					break;
+
+				case SDLK_RIGHT:
+					if (speedx > 0) {
+						speedx = 0;	}
+					break;
+
+				case SDLK_UP:
+					if (speedy < 0) {
+						speedy = 0;	}
+					break;
+
+				case SDLK_DOWN:
+					if (speedy > 0) {
+						speedy = 0; }
 					break;
 				}
 			}
-		/*	if (event.type == SDL_KEYUP) {
-				switch (event.key.keysym.sym) {
-				case SDLK_LEFT:
-					
-					break;
-				case SDLK_RIGHT:
-					
-					break;
-				case SDLK_UP:
-					if (ship.y < 0)
-						ship.y = 0;
-					break;
-				case SDLK_DOWN:
-					if (ship.y > 0)
-						ship.y = 0;
-					break;
-				default:
-					break;
-				}
-				break;
-			}*/
 		}
+		if (ship.x + speedx < 0 ){
+			ship.x = 0;
+		}
+		else if (ship.x + speedx > width) {
+			ship.x = width - 50;
+		}
+		ship.x += speedx;
+		if (ship.y + speedy < 0) {
+			ship.y = 0;
+		}
+		else if (ship.y + speedy > height) {
+			ship.y = height - 50;
+		}
+		ship.y += speedy;
 
 		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
 		SDL_RenderClear(renderer);
@@ -79,3 +91,30 @@ int main(int argc, char* argv[]) {
 
 	return 0;
 }
+//
+//keystate = SDL_GetKeyState(NULL);
+//if (keystate[SDLK_LEFT]) {
+//	rcSprite.x -= 2;
+//}
+//if (keystate[SDLK_RIGHT]) {
+//	rcSprite.x += 2;
+//}
+//if (keystate[SDLK_UP]) {
+//	rcSprite.y -= 2;
+//}
+//if (keystate[SDLK_DOWN]) {
+//	rcSprite.y += 2;
+//}
+///* collide with edges of screen */
+//if (rcSprite.x < 0) {
+//	rcSprite.x = 0;
+//}
+//else if (rcSprite.x > SCREEN_WIDTH - SPRITE_SIZE) {
+//	rcSprite.x = SCREEN_WIDTH - SPRITE_SIZE;
+//}
+//if (rcSprite.y < 0) {
+//	rcSprite.y = 0;
+//}
+//else if (rcSprite.y > SCREEN_HEIGHT - SPRITE_SIZE) {
+//	rcSprite.y = SCREEN_HEIGHT - SPRITE_SIZE;
+//}
