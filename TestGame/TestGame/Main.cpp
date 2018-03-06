48fa607a69098879f2ae77c9a17ed3daa723635e
DavidTello1 <36265057+DavidTello1@users.noreply.github.com>
2018-03-06 13:29:32 +0100

    Movement v1

--- TestGame/TestGame/Main.cpp
+++ TestGame/TestGame/Main.cpp
@@ -2,15 +2,15 @@
 #pragma comment(lib, "SDL/libx86/SDL2.lib")
 #pragma comment(lib, "SDL/libx86/SDL2main.lib")
 
-#define width 640
-#define height 640
+#define width 1200
+#define height 600
 
 int main(int argc, char* argv[]) {
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_Window *window = SDL_CreateWindow("SDL2TEST", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL);
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	SDL_Rect rect = { 0, 0, 64, 64 };
-
+	SDL_Rect ship = { 0, 0, 60, 60 };
+	int speed = 10;
 	bool isRunning = true;
 	SDL_Event event;
 
@@ -19,13 +19,57 @@ int main(int argc, char* argv[]) {
 			if (event.type == SDL_QUIT) {
 				isRunning = false;
 			}
+			if (event.type == SDL_KEYDOWN) {
+				switch (event.key.keysym.sym){
+				case SDLK_LEFT:
+					if (ship.x - speed > -10) {
+						ship.x -= speed;
+					}
+					break;
+				case SDLK_RIGHT:
+					if (ship.x + speed < 1200-50) {
+						ship.x += speed;
+					}
+					break;
+				case SDLK_UP:
+					if (ship.y - speed > -10) {
+						ship.y -= speed;
+					}
+					break;
+				case SDLK_DOWN:
+					if (ship.y + speed < 600-50) {
+						ship.y += speed;
+					}
+					break;
+				}
+			}
+		/*	if (event.type == SDL_KEYUP) {
+				switch (event.key.keysym.sym) {
+				case SDLK_LEFT:
+					
+					break;
+				case SDLK_RIGHT:
+					
+					break;
+				case SDLK_UP:
+					if (ship.y < 0)
+						ship.y = 0;
+					break;
+				case SDLK_DOWN:
+					if (ship.y > 0)
+						ship.y = 0;
+					break;
+				default:
+					break;
+				}
+				break;
+			}*/
 		}
+
 		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
 		SDL_RenderClear(renderer);
-
 		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-		SDL_RenderFillRect(renderer, &rect);
-
+		SDL_RenderFillRect(renderer, &ship);
 		SDL_RenderPresent(renderer);
 	}
 	
